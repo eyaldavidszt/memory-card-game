eb47abb601af1f2ee0c8ee35ac1c259118067594
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-09-20 07:56:12 +0300

    Add game end if reclick a card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect, Fragment } from "react";
-const seen = [];
+import "../styles/app.css";
+const clicked = [];
 let count = 0;
 const updateDisplay = (data) => {
   while (count < 1) {
@@ -8,14 +9,14 @@ const updateDisplay = (data) => {
     let display = [];
     let count = 0;
     while (count < 5) {
-      const randIndex = Math.floor(Math.random() * 1021);
+      const randIndex = Math.floor(Math.random() * 5);
       if (!indeces.includes(randIndex)) {
         indeces.push(randIndex);
         display.push(data[randIndex]);
         count += 1;
       }
     }
-    if (display.some((item) => !seen.includes(item))) {
+    if (display.some((item) => !clicked.includes(item.name))) {
       return display;
     }
   }
@@ -33,11 +34,11 @@ async function expandPoke(array) {
 export default function App() {
   const [pokeDisplay, setPokeDisplay] = useState([]);
   const [pokeData, setPokeData] = useState([]);
-
+  const [gameOver, setGameOver] = useState(false);
   useEffect(() => {
     (async function fetchPokemon() {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=1021&offset=0",
+        "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0",
         { mode: "cors" }
       );
       const pokeDataRaw = await response.json();
@@ -47,21 +48,33 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    (async function doSomething() {
+    (async function () {
       if (pokeData.length === 0) return;
       const myArr = await expandPoke(updateDisplay(pokeData));
       setPokeDisplay(myArr);
     })();
   }, [pokeData]);
-  console.log(pokeData);
+  console.log(pokeDisplay);
+  function handleClick({ currentTarget }) {
+    console.log(currentTarget.id);
+    if (clicked.includes(currentTarget.id)) setGameOver(true);
+    clicked.push(currentTarget.id);
+    (async function () {
+      if (pokeData.length === 0) return;
+      const myArr = await expandPoke(updateDisplay(pokeData));
+      setPokeDisplay(myArr);
+    })();
+  }
+  if (gameOver) return <div>bye</div>;
+
   return (
-    <>
+    <div className="card-wrapper">
       {pokeDisplay.map((poke) => (
-        <button key={poke.name}>
+        <button key={poke.name} onClick={handleClick} id={poke.name}>
           <h2>{poke.name}</h2>
           <img src={poke.sprites.front_default} alt="" />
         </button>
       ))}
-    </>
+    </div>
   );
 }
